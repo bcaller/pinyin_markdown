f979f7dc9ade0fef886226a23cca2ef2dfd469ba
bcaller <bcaller@users.noreply.github.com>
2016-02-12 23:17:05 +0000

    Unicode problems. Dropping python 2 for now so travis is happy.

--- setup.py
+++ setup.py
@@ -40,8 +40,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.4',
